67a4353d3fa23d6844447e9f10c7e09a07d613c7
TomasLukes <lukest@seznam.cz>
2023-05-14 20:56:32 +0200

    Add mock-up text for each section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
 
 export default function About() {
   return (
-    /* About */
-    <>
-      <h3>About me</h3>
-    </>
+    /* About section */
+    <section id="about" className="">
+      <h2>O mně</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, asperiores perferendis pariatur 
+        itaque praesentium dolores fugit corrupti soluta tempore voluptatum perspiciatis neque enim repellendus, 
+        est commodi rem maxime corporis? Odit!
+      </p>
+    </section>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
 
 export default function Hero() {
   return (
-    /* Hero */
-    <>
-      <h3>Hero</h3>
-    </>
+    /* Hero section */
+    <section id="hero" className="">
+      <div>
+        <h1>Tomáš Lukeš</h1>
+        <h3>Frontend React developer</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Quod adipisci totam ipsam nisi doloribus vel quis nostrum mollitia ad, 
+          dolor eligendi sequi illum esse fugit est quas reiciendis repellendus molestiae.
+        </p>
+        <a>IN</a>
+        <a>GH</a>
+      </div>
+
+      <div>
+        <h3>Tech stack</h3>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+      
+    </section>
   )
 }
