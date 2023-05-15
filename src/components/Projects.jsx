ef934932bd562f235c80a6d8ea96dea6fb2cdddf
TomasLukes <lukest@seznam.cz>
2023-05-15 09:52:55 +0200

    Add ProjectCardLg and ProjectCardSm components, with card mock-ups. Imported to Projects.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,5 @@
+import ProjectCardLg from "./ProjectCardLg"
+import ProjectCardSm from "./ProjectCardSm"
 
 export default function Projects() {
   return (
@@ -5,8 +7,20 @@ export default function Projects() {
     <section id="projects" >
       {/* Projects container */}
       <div className="container mx-auto py-16 px-8 ">
-        <h3>Projects</h3>
-        <h4>Scrimbra projects</h4>
+        <h2 className="text-2xl md:text-3xl font-bold">
+          Projects
+        </h2>
+        <ProjectCardLg />
+        <h3 className="text-lg md:text-lg font-semibold">
+          Scrimba solo projects
+        </h3>
+        {/* Scrimba projects cards container */}
+        <div>
+          {/* Scrimba project cards */}
+          <ProjectCardSm />
+          <ProjectCardSm />
+          <ProjectCardSm />
+        </div>
       </div>
     </section>
   )
