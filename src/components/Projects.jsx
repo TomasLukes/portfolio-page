fe028b39a75aa2faee65b1e6c42a0d9d717f461f
TomasLukes <lukest@seznam.cz>
2023-05-15 10:14:55 +0200

    Initial styling and props setup of ProjectCardSm

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,11 +15,27 @@ export default function Projects() {
           Scrimba solo projects
         </h3>
         {/* Scrimba projects cards container */}
-        <div>
+        <div className="flex flex-col gap-6
+          md:flex-row">
           {/* Scrimba project cards */}
-          <ProjectCardSm />
-          <ProjectCardSm />
-          <ProjectCardSm />
+          <ProjectCardSm 
+            name={'Triviathon'}
+            description={'Description 1'}
+            img={'triviathon-screenshot-desktop.png'}
+            url={'https://triviathon.tomaslukes.com/'}
+          />
+          <ProjectCardSm 
+            name={'Movie Search'} 
+            description={'Description 1'}
+            img={'moviesearch-screenshot-desktop.png'}
+            url={'https://moviesearch.tomaslukes.com/'}
+          />
+          <ProjectCardSm 
+            name={'Tenzies'} 
+            description={'Description 1'}
+            img={'tenzies-screenshot-desktop.png'}
+            url={'https://tenzies.tomaslukes.com/'}
+          />
         </div>
       </div>
     </section>
