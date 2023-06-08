bb2be02d10d63b72de06c7706b8637eb354c463e
TomasLukes <lukest@seznam.cz>
2023-06-08 17:57:21 +0200

    Implemented Formspree for contact form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,12 @@
+import { useForm, ValidationError } from "@formspree/react"
+
 export default function Contacts() {
+  const [state, handleSubmit] = useForm("moqzorbr");
+
+  if (state.succeeded) {
+    return <p>Děkuji za zprávu. Brzy se ozvu.</p>
+  }
+
   return (
     /* Contacts */
     <section id="contacts" className="pt-12 md:pt-24 pb-16 md:pb-32 px-8 md:px-16" >
@@ -7,7 +15,7 @@ export default function Contacts() {
       <h2 className="w-fit mx-auto text-2xl md:text-4xl font-bold mb-3 md:mb-9 border-solid border-l-8 pl-2 border-yellow-500">
         Kontakt
       </h2>
-        <form action="" className="grid grid-cols-1 gap-2 md:w-3/5 mx-auto">
+        <form action="" onSubmit={handleSubmit} className="grid grid-cols-1 gap-2 md:w-3/5 mx-auto">
           {/* Inputs group */}
           <div className="flex flex-row gap-2 md:gap-4">
             {/* Name input */}
@@ -15,10 +23,20 @@ export default function Contacts() {
              bg-neutral-50 w-full rounded-md py-2 pl-3 pr-3 shadow-sm border-2 border-gray-300
              focus:outline-none focus:border-yello-500 focus:ring-yellow-500 focus:ring-1 sm:text-sm" 
             />
+            <ValidationError 
+            prefix="Name" 
+            field="name"
+            errors={state.errors}
+            />
             {/* Email input */}
             <input type='email' name="email" placeholder="E-mail" className="placeholder:italic placeholder:text-slate-400 block
              bg-neutral-50 w-full rounded-md py-2 pl-3 pr-3 shadow-sm border-2 border-gray-300
              focus:outline-none focus:border-yello-500 focus:ring-yellow-500 focus:ring-1 sm:text-sm" 
+            />
+            <ValidationError 
+            prefix="Email" 
+            field="email"
+            errors={state.errors}
             />
           </div>
           {/* Message textarea */}
@@ -28,9 +46,14 @@ export default function Contacts() {
               focus:outline-none focus:border-yello-500 focus:ring-yellow-500 focus:ring-1 sm:text-sm md:mt-3" 
             >
             </textarea>
+            <ValidationError 
+            prefix="Message" 
+            field="message"
+            errors={state.errors}
+            />
           </div>
           {/* Submit button */}
-          <button className="mt-4 md:mt-6 m px-6 py-3 bg-neutral-900 text-neutral-100 rounded-lg border border-neutral-50
+          <button type="submit"  className="mt-4 md:mt-6 m px-6 py-3 bg-neutral-900 text-neutral-100 rounded-lg border border-neutral-50
             hover:border-yellow-500">
             Odeslat
           </button>
